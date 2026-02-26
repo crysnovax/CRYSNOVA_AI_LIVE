5b6ea384e9f9a48b9efa9a2d7052c9428d28f608
CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>
2026-02-26 15:47:15 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: { extend: { colors: { 'crys-dark': '#0a0a0f', 'crys-darker': '#060609', 'crys-blue': '#00d4ff', 'crys-purple': '#a855f7', 'crys-pink': '#ec4899' }}},
+  plugins: []
+}
